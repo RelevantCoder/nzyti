a4abeb09be32cd7d63276621188841b5c86b69f9
amzy-0 <azymiamin@gmail.com>
2020-02-26 13:47:18 +0330

    added feature: sorter for files and folder  in \'ls\' command

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import os 
-while True :
-    cmnd = input()
+while 1==1 :
+    cmnd = input("termX--> ")
 
     if "cd " in cmnd: 
         os.chdir(cmnd[3:])
         os.system("pwd")
-    elif cmnd == "cd ..":
+    elif cmnd == "..":
         os.chdir("..")
+    elif cmnd == "ls":
+        print("==========file(s)============\n")
+        os.system("ls -a -p | grep -v / ")
+        print("==========Folder(s)==========\n")
+        os.system("ls -d */ ")
+        print("=============================\n")
     else:
         os.system(cmnd)
+
