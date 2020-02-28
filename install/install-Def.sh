84a7e40782d22217d5eac2effe4b6c2fc1d69eac
amzy-0 <azymiamin@gmail.com>
2020-02-28 03:37:41 +0330

    this installer(install-Def.sh) , install zorich to be your defualt shell :) and you can use it if you want !

---
+++ install/install-Def.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo apt-get install dialog
+
+dialog --menu  "      || Welocome to Zorich installer ||" 25 50 25 "->" "install Zrich to be DEFAULT shell !"
+return_value=$?
+case $return_value in 
+        0)
+            cd .. 
+            cp main.py ~/.zorich.py
+            echo "alias zorich=\"python3 ~/.zorich.py\"" >> ~/.bashrc
+            echo "zorich" >> ~/.bashrc
+
+	    echo "alias zorich=\"python3 ~/.zorich.py\"" >> ~/.zshrc
+	    echo "zorich" >> ~/.zshrc
+	    sleep 7
+	    dialog --msgbox "zorich installed !\nThank's !!!" 27 40 	    
+	    clear
+esac
