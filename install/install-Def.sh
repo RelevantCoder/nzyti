928c8574b28565e456eeb7a8cfa8f5d9ecd70c90
amzy-0 <azymiamin@gmail.com>
2020-05-19 08:38:15 +0430

    installer updated

--- install/install-Def.sh
+++ install/install-Def.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
 sudo apt-get install dialog
+sudo dnf install dialog
+sudo yum install dialog 
+sudo pkg install dialog 
+sudo zipper install dialog
+clear
 
-dialog --menu  "      || Welocome to Zorich installer ||" 25 50 25 "->" "install Zrich to be your DEFAULT shell !"
+dialog --menu  "      || Welocome to Nzyti installer ||" 25 50 25 "->" "install Nzyti to be your DEFAULT shell !"
 return_value=$?
 case $return_value in 
         0)
             cd .. 
-            cp main.o ~/.zorich.o
-            echo "alias zorich=\"~/.zorich.o\"" >> ~/.bashrc
-            echo "zorich" >> ~/.bashrc
+            cp main.o ~/.nzyti.o
+            echo "alias nzyti=\"~/.nzyti.o\"" >> ~/.bashrc
+            echo "nzyti" >> ~/.bashrc
 
-	    echo "alias zorich=\"~/.zorich.o\"" >> ~/.zshrc
-	    echo "zorich" >> ~/.zshrc
+	    echo "alias nzyti=\"~/.nzyti.o\"" >> ~/.zshrc
+	    echo "nzyti" >> ~/.zshrc
 	    sleep 7
-	    dialog --msgbox "zorich installed !\nThank's !!!" 27 40 	    
+	    dialog --msgbox "Nzyti installed !\nThank's !!!" 27 40 	    
 	    clear
 esac
--- install/installer.sh
+++ install/installer.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
 sudo apt-get install dialog
+sudo dnf install dialog 
+sudo yum install dialog
+sudo zipper install dialog
+sudo pkg install dialog 
+clear
 
-dialog --menu  " || Welocome to Zorich installer ||" 25 40 25 "->" "install Zrich"
+dialog --menu  " || Welocome to Nzyti installer ||" 25 40 25 "->" "install Nzyti"
 return_value=$?
 case $return_value in 
         0)
             cd .. 
-            cp main.o  ~/.zorich.o
-            echo "alias zorich=\"~/.zorich.o\"" >> ~/.bashrc
-	    echo "alias zorich=\"~/.zorich.o\"" >> ~/.zshrc
+            cp main.o  ~/.nzyti.o
+            echo "alias nzyti=\"~/.nzyti.o\"" >> ~/.bashrc
+	    echo "alias nzyti=\"~/.nzyti.o\"" >> ~/.zshrc
 	    sleep 7
-	    dialog --msgbox "zorich installed !\nThank's !!!" 27 40 	    
+	    dialog --msgbox "Nzyti installed !\nThank's !!!" 27 40 	    
 	    clear
 esac
